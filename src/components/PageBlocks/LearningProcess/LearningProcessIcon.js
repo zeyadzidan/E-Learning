f903fb47d00661303cbe4832cbfee6c52cd7b14d
Zeyad Zidan <iamzeyadzidan@gmail.com>
2023-12-21 20:53:09 +0200

    Create LearningProcessIcon.js
    Refactored the learning process icons.

---
+++ src/components/PageBlocks/LearningProcess/LearningProcessIcon.js
@@ -0,0 +1,23 @@
+import { useTheme } from "@emotion/react";
+import { Stack, Typography } from "@mui/material";
+
+export default function LearningProcessIcon({ icon, text }) {
+    const mainTheme = useTheme();
+
+    return (
+        <Stack direction="column" spacing={2} alignItems="center" justifyContent="center">
+            <Stack
+                width="160px"
+                height="160px"
+                bgcolor="tertiary.main"
+                borderRadius="20%"
+                border={`2px solid ${mainTheme.palette.primary.main}`}
+                alignItems="center"
+                justifyContent="center"
+            >
+                {icon}
+            </Stack>
+            <Typography variant="h4" marginTop="20%">{text}</Typography>
+        </Stack>
+    )
+}
