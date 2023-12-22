3208c6d96f3783a1e76427bb228e122c6914cd21
Zeyad Zidan <iamzeyadzidan@gmail.com>
2023-12-23 00:29:05 +0200

    Update Categories.js

--- src/components/PageBlocks/Categories/Categories.js
+++ src/components/PageBlocks/Categories/Categories.js
@@ -3,18 +3,39 @@ import CategoryButtonsGroup from "./CategoryButtonGroup";
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import { useTheme } from "@emotion/react";
 import { useState } from "react";
+import CourseCardsList from "./CourseCard/CourseCardsList";
 
 export default function Categories({ categories }) {
     const mainTheme = useTheme();
 
+    // TODO: Handle this to fetch courses according to selected category.
     const [selectedCtg, setSelectedCtg] = useState('');
+
+    const courses = [
+        {
+            name: 'Introduction to Machine Learning',
+            tags: 'machine learning',
+            instructor: "Prof. Ayman Khalafallah",
+            rating: 1
+        },
+        {
+            name: 'Cracking the nut of security systems in a nutshell savjnkd ajksndvdjahbskjba djhkfb adkfjb djhkb adjhk fbh bsdahj badjhk bahkjabv djhk bahjk vadbh va',
+            tags: 'security systems',
+            instructor: "Prof. Ahmed Kosba",
+            rating: 5
+        },
+        { name: 'Computer Vision', tags: 'computer vision', instructor: "Prof. Marwan Torki", rating: 2 },
+        { name: 'Getting a life', tags: 'grass', instructor: "Prof. Zeyad Zidan", rating: 5 },
+        { name: 'Game Development', tags: 'video games', instructor: "Epic Games Professors", rating: 3 },
+        { name: 'How I Met Your Mother', tags: 'moms', instructor: "Prof. Abdelaziz Mohammed", rating: 4 },
+    ]
 
     return (
         <Stack
             direction="column"
             alignItems="center"
             width="100%"
-            height="1000px"
+            height="100%"
             bgcolor="tertiary.main"
             borderRadius="1%"
         >
@@ -26,6 +47,7 @@ export default function Categories({ categories }) {
             </Typography>
 
             <CategoryButtonsGroup categories={categories} />
+            <CourseCardsList courses={courses} />
 
             <Button
                 sx={{
