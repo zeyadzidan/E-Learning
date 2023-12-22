89bc55a2dd17aec94a7267327803ba8869dd26b3
Zeyad Zidan <iamzeyadzidan@gmail.com>
2023-12-23 00:29:23 +0200

    Update SearchBar.js

--- src/components/PageBlocks/Header/SearchBar/SearchBar.js
+++ src/components/PageBlocks/Header/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import { Button, FormControl, Stack, TextField } from "@mui/material";
+import { Button, Stack, TextField } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import { useState } from "react";
 
