330d1d76c76e1351ff70eaa20cde22bcc8651bbd
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-01-29 17:05:31 +0200

    Modified the App to include the Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import LandingPage from "./components/LandingPage";
+import Header from "./components/PageBlocks/Header/Header";
 
 export default function App() {
     return (
         <div>
             <LandingPage />
+            <Header />
         </div>
     )
 }
