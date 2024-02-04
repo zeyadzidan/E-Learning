61193b9ce9792ea7635cb79972e31e905aa269ff
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-02-04 23:09:49 +0200

    Added static certificates

---
+++ src/components/PageBlocks/Slideshow/certificates.js
@@ -0,0 +1,18 @@
+export const certificates = [
+    {
+        name: "Google IT Support Professional Certificate",
+        issuer: "Google"
+    },
+    {
+        name: "IBM Cybersecurity Analyst Professional Certificate",
+        issuer: "IBM"
+    },
+    {
+        name: "Cloud Security Engineer Professional Certificate",
+        issuer: "Google Cloud"
+    },
+    {
+        name: "IBM Cybersecurity Analyst Professional Certificate",
+        issuer: "IBM"
+    },
+]
