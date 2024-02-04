b27ce93b62eef5366772ab7623c2badc08fbb90a
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-02-04 23:08:21 +0200

    Update LandingPage.js
    Modified the categories of courses and certificates.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,32 +8,42 @@ import { Stack } from "@mui/material";
 
 export default function LandingPage() {
   const coursesCategories = [
-    { id: 1, name: "Development" },
-    { id: 2, name: "Design" },
-    { id: 3, name: "Marketing" },
-    { id: 4, name: "Business" },
-    { id: 5, name: "Languages" },
-    { id: 6, name: "Zayoda" },
+    { id: 0, name: "English" },
+    { id: 1, name: "Arabic" },
+    { id: 2, name: "Math" },
+    { id: 3, name: "Science" },
+    { id: 4, name: "Electronic Engineering" },
+    { id: 5, name: "Steam" },
+    { id: 6, name: "Social Studies" },
+    { id: 7, name: "Thermodynamics" },
   ];
 
   const certCategories = [
-    { id: 1, name: "IT Certificates" },
-    { id: 2, name: "Master Track Certificates" },
-    { id: 3, name: "University Certificates" },
-    { id: 4, name: "Master Certificates" },
-    { id: 5, name: "Ph.D. Certificates" },
-    { id: 6, name: "Zayoda" },
+    { id: 0, name: "IT Certificates" },
+    { id: 1, name: "Master Track Certificates" },
+    { id: 2, name: "University Certificates" },
+    { id: 3, name: "Master Certificates" },
+    { id: 4, name: "Ph.D. Certificates" },
   ];
 
   return (
     <Stack position="absolute" top="105px" right="0" left="0">
       <Intro />
       <LearningProcess />
-      <Slideshow categories={coursesCategories} isForCourses displayedCategories={5} displayedCards={4} />
+      <Slideshow
+        categories={coursesCategories}
+        isForCourses
+        displayedCategories={5}
+        displayedCards={4}
+      />
       <Benefits />
       <Slideshow isForCourses byRating displayedCards={3} />
       <Clients />
-      <Slideshow categories={certCategories} displayedCategories={4} displayedCards={4} />
+      <Slideshow
+        categories={certCategories}
+        displayedCategories={4}
+        displayedCards={4}
+      />
       <Footer />
     </Stack>
   );
