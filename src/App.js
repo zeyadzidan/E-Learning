e8f1852eebf00d7935df6b57993c754747843217
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-02-04 23:09:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import LandingPage from "./components/LandingPage";
 import Header from "./components/PageBlocks/Header/Header";
+import { fetchCoursesByCriteria } from "./fetchCoursesByCriteria";
 
 export default function App() {
-    return (
-        <div>
-            <LandingPage />
-            <Header />
-        </div>
-    )
+
+    useEffect(() => {
+        (async () => {
+          await fetchCoursesByCriteria("English");
+        })();
+      }, []);
+
+  return (
+    <div>
+      <LandingPage />
+      <Header />
+    </div>
+  );
 }
